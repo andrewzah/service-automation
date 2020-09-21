cb04745ca24b0e41107ce47af19b77e4a9a2ceab
Andrew Zah <zah@andrewzah.com>
2020-09-21 14:04:38 -0400

    update go_bots -> misc_bots, add mpd,mattermost

--- services/goatcounter/entrypoint.sh
+++ services/goatcounter/entrypoint.sh
@@ -2,7 +2,7 @@
 
 goatcounter serve -port "${GC_PORT}" \
   -listen "${GC_LISTEN}" \
-  -db "${GC_DB}" \
+  -db "/data/${GC_DB}" \
   -tls "${GC_TLS}" \
   -errors "${GC_ERRORS}" \
   -email-from "${GC_EMAIL_FROM}" \
--- services/postgres/docker-entrypoint-initdb.d/huginn.sh
+++ services/postgres/docker-entrypoint-initdb.d/huginn.sh
@@ -4,6 +4,8 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER huginn;
+    ALTER USER huginn WITH CREATEDB;
+
     CREATE DATABASE huginn;
     GRANT ALL PRIVILEGES ON DATABASE huginn TO huginn;
 EOSQL
