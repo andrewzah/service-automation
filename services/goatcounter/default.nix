56e574f2675dcdbf08b6cac6e19e49f5fffb7a7e
Andrew Zah <zah@andrewzah.com>
2024-10-02 14:06:58 +0900

    changes......

---
+++ services/goatcounter/default.nix
@@ -0,0 +1,64 @@
+{ pkgs ? import <nixpkgs>{} }:
+let
+  goatcounter = pkgs.buildGoModule rec {
+    pname = "goatcounter";
+    version = "530ab5edff553923fb04d9e1b1a9771f8a6d0461";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "arp242";
+      repo = "goatcounter";
+      rev = version;
+      hash = "sha256-8n6d89pm8Itm9OodF3SdXplOKf3ocxSWgZ+VE7FyX68=";
+    };
+
+    preBuild = ''
+      sed -i 's#1.23.0#1.22.0#' go.mod
+    '';
+
+    vendorHash = "sha256-8W/xQ8jkNjjmaAvdoY/66HCW7dA+pFC4MVc17J/3B5o=";
+    subPackages = [ "cmd/goatcounter" ];
+    modroot = ".";
+
+    allowReference = true;
+    ldflags = [
+      "-s"
+      "-w"
+      "-X zgo.at/goatcounter.Version=${version}"
+    ];
+
+    meta = {
+      description = "Easy web analytics. No tracking of personal data. ";
+      mainProgram = "goatcounter";
+      homepage = "https://github.com/arp242/goatcounter";
+      license = pkgs.lib.licenses.eupl12;
+    };
+  };
+
+  entrypoint = pkgs.writeShellScript "entrypoint.sh" ''
+    ${goatcounter}/bin/goatcounter serve \
+      -port "$${GC_PORT}" \
+      -listen "$${GC_LISTEN}" \
+      -db "$${GC_DB}" \
+      -tls "$${GC_TLS}" \
+      -errors "$${GC_ERRORS}" \
+      -email-from "$${GC_EMAIL_FROM}" \
+      -smtp "$${GOATCOUNTER_SMTP}" \
+      -automigrate
+  '';
+in {
+  img = pkgs.dockerTools.buildImage {
+    name = "docker.io/andrewzah/goatcounter";
+    tag = "${goatcounter.version}";
+
+    #copyToRoot = pkgs.buildEnv {
+    #  name = "img-root";
+    #  paths = [ entrypoint ];
+    #  pathsToLink = [ "/bin" ];
+    #};
+
+    config = {
+      entrypoint = [ "${entrypoint}" ];
+      exposedPorts = { "3443" = {}; };
+    };
+  };
+}
--- todo.md
+++ todo.md
@@ -1,1 +1,34 @@
-https://usememos.com/
+## priority 1
+- restic scripts
+
+## icebox
+- changedetection.zah.rocks
+  - https://github.com/dgtlmoon/changedetection.io
+  - vs windmill / huginn ?
+  - + playwright: https://github.com/microsoft/playwright
+- glance.zah.rocks
+  - https://github.com/glanceapp/glance
+- archive.zah.rocks
+  - ? https://github.com/hoarder-app/hoarder
+- images.zah.rocks
+  - https://github.com/immich-app/immich
+- status.zah.rocks
+  https://github.com/harsxv/tinystatus
+  https://news.ycombinator.com/item?id=41452339
+- https://github.com/apache/incubator-answer
+- airtracking.zah.rocks
+  https://github.com/johanohly/AirTrail
+- barassistant.zah.rocks
+  - https://github.com/karlomikus/bar-assistant
+- firefly/finances.zah.rocks ?
+  - https://selfh.st/newsletter/2024-09-27
+- https://github.com/cioraneanu/firefly-pico
+- storage.zah.rocks (LXC accessible by friends SSH)
+- nix install ?
+- manga.zah.rocks
+  - https://github.com/diogovalentte/mantium vs mango/etc
+- 3dmodels.zah.rocks
+  - https://try.manyfold.app
+
+## completed
+- memos.zah.rocks - https://usememos.com/
